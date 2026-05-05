81d4344d8476c701d657fa840e2b17b7c3db48c0
o-ankomochi-o <guugurunomeeru@gmail.com>
2026-05-05 10:42:05 +0900

    test: add stem generator spec

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,6 @@
-from core import build_caption
+from datetime import datetime
+
+from core import build_caption, make_stem
 
 
 def test_build_caption_combines_trigger_and_caption():
@@ -11,3 +13,8 @@ def test_build_caption_omits_empty_trigger_word():
 
 def test_build_caption_omits_empty_caption():
     assert build_caption(trigger_word="trg", caption="") == "trg"
+
+
+def test_make_stem_formats_timestamp_and_index():
+    stem = make_stem(timestamp=datetime(2026, 5, 5, 12, 34, 56), index=1)
+    assert stem == "20260505_123456_001"
